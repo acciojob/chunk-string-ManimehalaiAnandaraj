e553d5fec4011ad5e327051b31278d863ff56dec
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-21 12:32:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	if(!str)
+	{
+		return [];
+	}
+	const chunk=[];
+	for(let i=0;i<str.length;i += size)
+		{
+			chunk.push(str.substring(i,i+size));
+		}
+	return chunk;
 }
 
 // Do not change the code below
